10a6151d1ee28e78a1a00612a45fbededc5d6687
Gabriel Holanda <gabriel1996hp@hotmail.com>
2024-01-15 11:51:42 -0300

    fix: limitação de casa decimais

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ function atualizarTabela() {
 function atualizarMediaFinal() {
     const mediaFinal = calcularMediaFinal()
 
-    document.getElementById("media-final-valor").innerHTML = mediaFinal
+    document.getElementById("media-final-valor").innerHTML = mediaFinal.toFixed(2)
     document.getElementById("media-final-resultado").innerHTML = mediaFinal >= 7 ? spanAprovado : spanReprovado
 
 
